a9d0a199a40e2b172d917a4c16066174aeee8aa0
annphil <75479202+annphil@users.noreply.github.com>
2023-08-08 15:16:22 +0530

    Create FTPClient.c

---
+++ FTPClient.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+int main()
+{
+    FILE *fp;
+    int csd, n, ser, s, cli, cport, newsd;
+    char name[100], rcvmsg[100], rcvg[100], fname[100];
+    struct sockaddr_in servaddr;
+    
+    printf("Enter the port");
+    scanf("%d", &cport);
+    csd = socket(AF_INET, SOCK_STREAM, 0);
+    if (csd < 0)
+    {
+        printf("Error....\n");
+        exit(0);
+    }
+    else
+        printf("Socket is created\n");
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr =inet_addr("127.0.0.1");
+    servaddr.sin_port = htons(cport);
+    if (connect(csd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+        printf("Error in connection\n");
+    else
+        printf("connected\n");
+        
+    printf("Enter the name file whose content is to be fetched by server\t");
+    scanf("%s", name);
+    printf("Enter the new file name to which server content is to be written\t");
+    scanf("%s", fname);
+    fp = fopen(fname, "w");
+    send(csd, name, sizeof(name), 0);
+    while (1)
+    {
+        s = recv(csd, rcvg, 100, 0);
+
+        rcvg[s] = '\0';
+        if (strcmp(rcvg, "error") == 0)
+            printf("File is not available\n");
+        if (strcmp(rcvg, "completed") == 0)
+        {
+            printf("File is transferred........\n");
+            fclose(fp);
+            close(csd);
+            break;
+        }
+        else
+            fputs(rcvg, stdout);
+        fprintf(fp, "%s", rcvg);
+        
+    }
+    return 0;
+    close(csd);
+}
